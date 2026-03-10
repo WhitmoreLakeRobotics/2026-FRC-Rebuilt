59b3d19b6264cb6d1e5f2bd4073a34bf3623a7d9
Jeff Schwennesen <xjschwen@gmail.com>
2026-03-10 16:28:19 -0400

    jas added a cosine function to LTurretCalc.java

--- src/main/java/frc/robot/subsystems/LTurretCalc.java
+++ src/main/java/frc/robot/subsystems/LTurretCalc.java
@@ -9,7 +9,11 @@ public class LTurretCalc {
     }
 
     public double getSinRPM(double degrees) {
-        return Math.sin(Math.toRadians(degrees /2)) * baseRPM;
+        return Math.sin(Math.toRadians(degrees / 2)) * baseRPM;
+    }
+
+    public double getCosRPM(double degrees) {
+        return (1 - Math.cos(Math.toRadians(degrees))/2) * baseRPM;
     }
 
 
@@ -20,10 +24,10 @@ public class LTurretCalc {
     public static void main(String... args) {
 
         LTurretCalc TC = new LTurretCalc();
-        TC.setBaseRPM(50);
+        TC.setBaseRPM(12.5);
 
         for (int i = (int) 0; i < 360 + 1; i++) {
-            System.out.printf(" %4.2f     : %.2f%n", (double)i, TC.getSinRPM(i));
+            System.out.printf(" %4.2f     : %.2f%n", (double) i, TC.getCosRPM(i) - TC.getCosRPM(0));
         }
     }
 }
